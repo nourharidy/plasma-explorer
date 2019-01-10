54539b308fec378c040ede672083286f564edfa0
Kelvin Fichter <kelvinfichter@gmail.com>
2019-01-09 20:06:13 -0500

    Added single block page

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,57 +1,108 @@
 <template>
-  <v-container
-    id="grid"
-    grid-list-md
-  >
-    <v-layout row wrap>
-      <v-flex>
-        <div class="content elevation-5">
-          <h2 class="item-hash">Block <span class="font-weight-light">#{{ $route.params.number }}</span></h2>
-          <v-layout row wrap>
-            <v-flex xs12 md2 class="property-title">
-              Number:
-            </v-flex>
-            <v-flex xs12 md10>
-              {{ $route.params.number }}
-            </v-flex>
-
-            <v-flex xs12 md2 class="property-title">
-              Hash:
-            </v-flex>
-            <v-flex xs12 md10>
-              {{ block.hash }}
-            </v-flex>
-
-            <v-flex xs12 md2 class="property-title">
-              Transactions:
-            </v-flex>
-            <v-flex xs12 md10>
-              {{ block.transactions }}
-            </v-flex>
-          </v-layout>
+  <div>
+    <div class="view-header">Block #{{ block.number }}</div>
+    <div class="single-info">
+      <div class="rainbow-left"></div>
+      <div class="info-block">
+        <div class="info-point">
+          <div class="info-data">{{ block.transactions }}</div>
+          <div class="info-title">Transactions</div>
         </div>
-      </v-flex>
-    </v-layout>
-  </v-container>
+        <div class="info-point">
+          <div class="info-data">{{ block.timestamp }}</div>
+          <div class="info-title">Timestamp</div>
+        </div>
+        <div class="info-point">
+          <div class="info-data">{{ block.size }}</div>
+          <div class="info-title">Size in Bytes</div>
+        </div>
+      </div>
+      <div class="vertical-divider"></div>
+      <div class="info-block">
+        <div class="info-point">
+          <div class="info-data break-word">{{ block.operator }}</div>
+          <div class="info-title">Operator Address</div>
+        </div>
+        <div class="info-point">
+          <div class="info-data break-word">{{ block.hash }}</div>
+          <div class="info-title">Block Hash</div>
+        </div>
+      </div>
+    </div>
+    <div class="view-header">Transactions</div>
+    <table>
+      <tr class="table-header">
+        <th v-for="header in headers" :key="header.value">
+          {{ header.text }}
+        </th>
+      </tr>
+      <router-link tag="tr" v-for="tx in txs" :key="tx.hash" :to="{ name: 'transaction', params: { hash: tx.hash } }" class="table-row">
+        <td class="blue-link"><div class="rainbow-left"></div>{{ tx.hash }}</td>
+        <td>{{ tx.timestamp }}</td>
+        <td>{{ tx.transfers }}</td>
+      </router-link>
+    </table>
+  </div>
 </template>
 
 <script>
-  import plasma from '../services/client-service'
+import plasma from '../services/client-service'
 
-  export default {
-    data () {
-      return {
-        block: undefined
-      }
-    },
-    mounted () {
-      plasma
-        .getBlock(this.$route.params.number)
-        .then((block) => {
-          this.block = block
+const ITEMS_PER_PAGE = 10
+
+export default {
+  data () {
+    return {
+      headers: [
+        {
+          text: 'TXID',
+          value: 'hash'
+        },
+        {
+          text: 'Timestamp',
+          value: 'timestamp'
+        },
+        {
+          text: 'Transfers',
+          value: 'transfers'
+        }
+      ],
+      block: {},
+      txs: []
+    }
+  },
+  watch: {
+    '$route.query.page': function () {
+      this.loadTransactions()
+    }
+  },
+  mounted () {
+    const number = parseInt(this.$route.params.number)
+
+    plasma.getBlock(number).then((block) => {
+      this.block = block
+    })
+
+    this.loadTransactions()
+  },
+  methods: {
+    loadTransactions () {
+      const number = parseInt(this.$route.params.number)
+      this.page = parseInt(this.$route.query.page) || 1
+      const start = (this.page - 1) * ITEMS_PER_PAGE
+      const end = this.page * ITEMS_PER_PAGE
+
+      plasma.getTransactionsInBlock(number, start, end).then((txs) => {
+        this.txs = txs.reverse()
+        this.txs = this.txs.map((tx) => {
+          tx.timestamp = 12392139123
+          tx.transfers = 0
+          return tx
         })
+      })
     }
   }
+}
 </script>
 
 <style>
--- src/components/Blocks.vue
+++ src/components/Blocks.vue
@@ -7,18 +7,25 @@
           {{ header.text }}
         </th>
       </tr>
-      <tr v-for="block in blocks" :key="block.number" class="table-row">
-        <td class="blue-link"><div class="table-rainbow"></div>{{ block.number }}</td>
+      <router-link tag="tr" v-for="block in blocks" :key="block.number" :to="{ name: 'block', params: { number: block.number } }" class="table-row">
+        <td class="blue-link"><div class="rainbow-left"></div>{{ block.number }}</td>
         <td>{{ block.size }}</td>
         <td>{{ block.timestamp }}</td>
         <td>{{ block.transactions }}</td>
-      </tr>
+      </router-link>
     </table>
+    <div class="pagination">
+      <router-link :to="{ query: { page: (page - 1) }}" class="page-prev" v-bind:class="{ disabled: (page === 1) }">&lt;</router-link>
+      <div class="page-current">{{ page }} of {{ maxPage }}</div>
+      <router-link :to="{ query: { page: (page + 1) }}" class="page-next" v-bind:class="{ disabled: (page === maxPage) }">&gt;</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 import plasma from '../services/client-service'
+
+const ITEMS_PER_PAGE = 10
 
 export default {
   data () {
@@ -41,15 +48,33 @@ export default {
           value: 'transactions'
         }
       ],
-      blocks: []
+      blocks: [],
+      page: 1,
+      maxPage: 1
+    }
+  },
+  watch: {
+    '$route.query.page': function () {
+      this.loadBlocks()
     }
   },
   mounted () {
-    plasma
-      .getBlocks(0, 10)
-      .then((blocks) => {
+    this.loadBlocks()
+
+    plasma.getHeight().then((height) => {
+      this.maxPage = Math.ceil(height / ITEMS_PER_PAGE)
+    })
+  },
+  methods: {
+    loadBlocks () {
+      this.page = parseInt(this.$route.query.page) || 1
+      const start = (this.page - 1) * ITEMS_PER_PAGE
+      const end = this.page * ITEMS_PER_PAGE
+
+      plasma.getBlocks(start, end).then((blocks) => {
         this.blocks = blocks.reverse()
       })
+    }
   }
 }
 </script>
